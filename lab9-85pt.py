47f167e39e6e64c781d5878379c9340f3f8e1949
TyBounds <tybounds@live.com>
2014-10-15 15:32:54 -0500

    85pt version done

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -6,11 +6,11 @@
 
 # Create a for loop that will search through temperatureList
 # and create a new list that includes only numbers greater than 100
-
 myList = [102,98,96,101,100,99,103,97,98,105]
+myList2 = []
 
-# Insert for loop here
-
-
-# This should print [102,101,103,105]
-print myList
+for i in myList:
+    if i >= 100:
+        myList2.append(i)
+print myList2
+    
